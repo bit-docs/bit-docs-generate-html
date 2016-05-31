726c94db286527f93ca2a902de1a49c4c0dc0154
Justin Meyer <justinbmeyer@gmail.com>
2016-05-31 16:43:23 -0500

    updating lodash

--- generate.js
+++ generate.js
@@ -9,35 +9,35 @@ var build = require("./build/build"),
 /**
  * @function documentjs.generators.html.generate
  * @parent documentjs.generators.html.methods
- * 
+ *
  * Generates an HTML site for a [documentjs.process.docMap docMap]
- * given configuration options.
- * 
- * @signature `.generate(docMapPromise, options)`
- * 
- * @param {Promise<documentjs.process.docMap>} docMapPromise A promise that 
+ * given configuration siteConfig.
+ *
+ * @signature `.generate(docMapPromise, siteConfig)`
+ *
+ * @param {Promise<documentjs.process.docMap>} docMapPromise A promise that
  * contains a `docMap` created by [documentjs.process.files].
- * @param {Object} options Configuration options.
- * 
+ * @param {Object} siteConfig Configuration siteConfig.
+ *
  * @return {Promise} A promise that resolves when the site has been built.
  */
-module.exports = function(docMapPromise, options){
+module.exports = function(docMapPromise, siteConfig){
 	// 1. Copies everything from site/default/static to site/static/build
-	// 2. Overwrites site/static/build with content in `options.static`
+	// 2. Overwrites site/static/build with content in `siteConfig.static`
 	// 3. Runs site/static/build/build.js
 	//    A. Builds itself and copies everything to site/static/dist
-	var staticPromise = build.staticDist(options).then(function(){
+	var staticPromise = build.staticDist(siteConfig).then(function(){
 		// copies statics to documentation location.
-		return write.staticDist(options);
+		return write.staticDist(siteConfig);
 	});
-	
-	var buildTemplatesPromise = build.templates(options).then(function(){
+
+	var buildTemplatesPromise = build.templates(siteConfig).then(function(){
 		return Handlebars.create();
 	});
 	buildTemplatesPromise["catch"](function(){
 		console.log("problem building templates");
 	});
-	
+
 	var currentDocObject;
 	var getCurrent = function(){
 		return currentDocObject;
@@ -46,18 +46,18 @@ module.exports = function(docMapPromise, options){
 		currentDocObject = current;
 	};
 	var helpersReadyPromise = docMapPromise.then(function(docMap){
-		return build.helpers(buildTemplatesPromise, docMap, options, getCurrent);
+		return build.helpers(buildTemplatesPromise, docMap, siteConfig, getCurrent);
 	});
-		
+
 	var docsPromise = Q.all([
 			docMapPromise,
-			build.renderer(buildTemplatesPromise, options),
+			build.renderer(buildTemplatesPromise, siteConfig),
 			helpersReadyPromise,
-			mkdirs(options.dest)
+			mkdirs(siteConfig.dest)
 	]).then(function(results){
 		var docMap = results[0],
 			renderer = results[1];
-		return write.docMap(docMap, renderer, options, setCurrent);
+		return write.docMap(docMap, renderer, siteConfig, setCurrent);
 	});
 	return Q.all([staticPromise, docsPromise]);
 };
