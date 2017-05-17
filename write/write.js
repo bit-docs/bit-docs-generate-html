c108352a065889b02750265000da4cc55e168c44
Mick McGrath <mick.mc.13@gmail.com>
2017-05-17 12:00:17 -0500

    #32 - remove search related stuff

--- write/write.js
+++ write/write.js
@@ -14,5 +14,3 @@
 exports.docMap = require("./doc_map");
 exports.docObject = require("./doc_object");
 exports.staticDist = require("./static_dist");
-exports.searchMap = require("./search_map");
-exports.docMapHash = require("./doc_map_hash");
