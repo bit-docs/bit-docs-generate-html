ee77b1289407fa31f57a6e6b1055ef7415b2c857
imaustink <austin@austinkurpuis.com>
2017-07-10 10:06:58 -0700

    Bassics of copy staticDist wroking

--- site/default/static/build.js
+++ site/default/static/build.js
@@ -1,51 +1,56 @@
-var stealTools = require("steal-tools"),
-	fsx = require('../../../../fs_extras'),
-	Q = require('q'),
-	path = require("path");
-
+var stealTools = require("steal-tools");
+var fsExtra = require('fs-extra');
+var fsx = require('../../../../fs_extras');
+var Q = require('q');
+var path = require("path");
+var copy = Q.denodeify(fsExtra.copy);
 
 module.exports = function(options, folders){
-
 	var copyDir = function(name){
-		return fsx.mkdirs( path.join(folders.dist,name) ).then(function(){
+		return fsx.mkdirs(path.join(folders.dist,name)).then(function(){
 			return fsx.exists(path.join(folders.build,name)).then(function(exists){
 				if(exists) {
-					return fsx.copy( path.join(folders.build,name), path.join(folders.dist,name) );
+					return fsx.copy(path.join(folders.build,name), path.join(folders.dist,name));
 				}
 			});
 		});
 	};
 
 	var staticDistPromises = [];
-	options.html.staticDist.forEach(function(dist){
-		staticDistPromises.push(fsx.copy(dist, path.join(folders.dist)));
-	});
+	if(options.html && options.html.staticDist){
+		options.html.staticDist.forEach(function(dist){
+			staticDistPromises.push(copy(path.join(process.cwd(), dist), folders.dist));
+		});
+	}
 
 	if(options.devBuild) {
 		// copy all dependencies
 		var promise = Q.all([
-			fsx.copy(path.join(folders.build), path.join(folders.dist) )
+			fsx.copy(path.join(folders.build), path.join(folders.dist))
 		].concat(staticDistPromises));
 		// copy everything and steal.js
 		return promise;
 	} else {
 
-		Promise.all(staticDistPromises).then(function(){
-			// run steal-tools and then copy things
-			return stealTools.build({
-				config: __dirname+"/package.json!npm",
-				main: "bit-docs-site/static"
-			},{
-				minify: options.minifyBuild === false ? false : true,
-				quiet: options.debug ? false : true,
-				debug: options.debug ?  true : false,
-				bundleAssets: true
-			});
+		// run steal-tools and then copy things
+		return stealTools.build({
+			config: __dirname+"/package.json!npm",
+			main: "bit-docs-site/static"
+		},{
+			minify: options.minifyBuild === false ? false : true,
+			quiet: options.debug ? false : true,
+			debug: options.debug ?  true : false,
+			bundleAssets: true
 		}).then(function(){
 			if(options.debug) {
 				console.log("BUILD: Copying build to dist.");
 			}
-			return fsx.copy(path.join(folders.build, "dist"), path.join(folders.dist) );
+
+			var promise = Q.all([
+				fsx.copy(path.join(folders.build, "dist"), path.join(folders.dist))
+			].concat(staticDistPromises));
+			
+			return promise;
 		});
 	}
 };
