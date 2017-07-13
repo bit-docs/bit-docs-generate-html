afbc7d8df878cb929d1f9e91dc91f791b1b12032
Joel Kuzmarski <leoj3n@gmail.com>
2017-07-13 18:03:00 -0500

    Update docs for /write

--- write/doc_object.js
+++ write/doc_object.js
@@ -1,36 +1,38 @@
-var _ = require("lodash"),
-	filename = require("./filename"),
-	Q = require('q'),
-	fs = require("fs"),
-	writeFile = Q.denodeify(fs.writeFile),
-	path = require("path");
-	mkdirs = Q.denodeify(require("fs-extra").mkdirs);
+var _ = require("lodash");
+var filename = require("./filename");
+var Q = require('q');
+var fs = require("fs");
+var writeFile = Q.denodeify(fs.writeFile);
+var path = require("path");
+var mkdirs = Q.denodeify(require("fs-extra").mkdirs);
+
 /**
- * @function documentjs.generators.html.write.docObject
- * @parent documentjs.generators.html.write.methods
+ * @parent bit-docs-generate-html/modules
+ * @module {function} bit-docs-generate-html/write/doc_object
  *
- * Writes out a [documentjs.process.docObject docObject].
+ * Writes out a [bit-docs/types/docObject].
  *
- * @signature `.write.docObject(docObject, renderer, siteConfig, setCurrentDocObjectForHelpers)`
+ * @signature `docObject(docObject, renderer, siteConfig, setCurrentDocObjectForHelpers)`
  *
- * @param {documentjs.process.docObject} docObject The doc object to be written out.
+ * @param {bit-docs/types/docObject} docObject The [bit-docs/types/docObject]
+ * to be written out.
  *
- * @param {documentjs.generators.html.types.renderer} renderer A function that renders
- * the output.
+ * @param {bit-docs-generate-html/types/renderer} renderer A function that
+ * renders the output.
  *
  * @param {Object} siteConfig Configuration siteConfig.
  *
- * @option {String} dest The folder name this file will be written to. The
- * filename is determined from the docObject's name.
+ *   @option {String} dest The folder name this file will be written to. The
+ *   filename is determined from the [bit-docs/types/docObject]'s name.
  *
- * @param {function(documentjs.process.docObject)} setCurrentDocObjectForHelpers
+ * @param {function(bit-docs/types/docObject)} setCurrentDocObjectForHelpers
  *
  * @return {Promise} A promise that resolves when the file has been written out.
  */
 module.exports = function(docObject, renderer, siteConfig, setCurrentDocObjectForHelpers){
-
 	var out = path.join(siteConfig.dest, filename(docObject, siteConfig) );
 	var rendered;
+
 	if(siteConfig.debug) {
 		console.log('OUT: ' + path.relative(process.cwd(),out) );
 	}
@@ -43,10 +45,10 @@ module.exports = function(docObject, renderer, siteConfig, setCurrentDocObjectFo
 	} else {
 		rendered = renderer(docObject);
 	}
+	
 	return writeFile(out, rendered).catch(function(){
 		return mkdirs(path.dirname(out)).then(function(){
 			return writeFile(out, rendered);
 		});
 	});
-
 };
